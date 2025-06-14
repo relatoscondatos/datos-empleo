a18897fe85f0082b80a2c3eea93af80c6402f5cb
Ernesto Laval <ernesto.laval@gmail.com>
2025-06-13 23:20:26 -0400

    Update observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,7 +1,9 @@
+
+
 // See https://observablehq.com/framework/config for documentation.
 export default {
   // The app’s title; used in the sidebar and webpage titles.
-  title: "Datos Empleo",
+  title: "Explorador de Indicadores de Empleo en Chile",
 
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
@@ -17,22 +19,21 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: '<link rel="icon" href="favicon.ico" type="image/png" sizes="32x32">',
 
   // The path to the source root.
   root: "src",
 
   // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
-  // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
-sidebar: false, // whether to show the sidebar
-  // toc: true, // whether to show the table of contents
+  theme: "default", // try "light", "dark", "slate", etc.
+  header: 'Un relato con datos <img src="./favicon.ico" height="20px"> <a href="https://www.relatoscondatos.cl" target="_blank">www.relatoscondatos.cl</a> ', // what to show in the header (HTML)
+  footer: 'Relatos con datos <i class="fas fa-envelope"></i> <a href="mailto:contacto@relatoscondatos.cl">contacto@relatoscondatos.cl</a>', // what to show in the footer (HTML)
+  sidebar: false, // whether to show the sidebar
+  toc: {show:false, label:"Contenido"}, // whether to show the table of contents
   pager: false, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
-  // typographer: false, // smart quotes and other typographic improvements
-  // preserveExtension: false, // drop .html from URLs
-  // preserveIndex: false, // drop /index from URLs
+  typographer: true, // smart quotes and other typographic improvements
+  // cleanUrls: true, // drop .html from URLs
 };
